336f9e73474e649a16bb617f1c80399f0496a1ed
Benjamin Kaplan <benjaminkaplanart@gmail.com>
2018-07-20 11:07:47 -0600

    created index view and started login logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const User = require("#");
+const bcrypt = reuqire("bycrypt");
 
 router.get("/", (req, res) => {
-	res.render("auth/login.ejs", {
+	res.render("auth/register.ejs", {
 		message: req.session.message
 	});
 });
+// router.post("/register", (req, res, next) => {
+// 	const password = req.body
+// });
 router.post("/login", (req, res) => {
 	console.log(req.session);
 	req.session.loggedIn = true;
 	req.session.username = req.body.username;
-	res.redirect("/articles")
+	res.redirect("/");
 });
 router.get("/logout", (req, res) => {
 	req.session.destroy((err) => {
