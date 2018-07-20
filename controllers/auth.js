995d70c8c7e3107759105056fbe2788b27f868bb
Laurie Carano <lauriecarano@gmail.com>
2018-07-20 11:53:52 -0600

    added to models and corrected mispell in auth-controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("#");
-const bcrypt = reuqire("bycrypt");
+const bcrypt = require("bycrypt");
 
 router.get("/", (req, res) => {
 	res.render("auth/register.ejs", {
