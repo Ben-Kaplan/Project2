c7153f85281524002ee9298500d61fc8721038e0
Laurie Carano <lauriecarano@gmail.com>
2018-07-25 14:42:35 -0600

    updated rest show page created gallery

--- models/restaurant.js
+++ models/restaurant.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const restaurantSchema = new mongoose.Schema({
 	name: {type: String, required: true}, 
-
+	zipcode: String,
 	restaurantLocation: String,
 	typeOfRestaurant: String,
 	price: Number,
 	comment: String,
 	rating: Number,
-	website: String
+	website: String,
+	reviews: []
+
 	//dogFriendly: Boolean
 });
 
