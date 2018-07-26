2af8beee3b67d41d780ba114a91d795230c13cdb
Benjamin Kaplan <benjaminkaplanart@gmail.com>
2018-07-26 15:58:29 -0600

    added some homepage styles

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const User  = require('../models/user');
+const User = require('../models/user');
 const Restaurant = require('../models/restaurant');
 router.get("/", async (req, res) => {
   try {
